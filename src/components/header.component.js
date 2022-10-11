afcc4de9220b9d0a46e6f26a8a8b6555790a9a38
MaxBasharin <maxbasharinweb@gmail.com>
2022-10-11 14:58:37 +0300

    первоначальная настройка проекта / старт верстки

---
+++ src/components/header.component.js
@@ -0,0 +1,16 @@
+import {Component} from '../core/component.js'
+
+export class HeaderComponent extends Component {
+	constructor(id) {
+		super(id)
+	}
+
+	init() {
+		const btn = this.$el.querySelector('.btn-header')
+		btn.addEventListener('click', buttonHandler.bind(this))
+	}
+}
+
+function buttonHandler() {
+	this.hide()
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { HeaderComponent } from "./components/header.component.js";
+
+const header = new HeaderComponent('header')
+
